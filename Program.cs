20b436ede0c38980ba2626127fa50979ad3fd755
school topic 4 <339719437@2260I-L29-031>
2025-10-06 09:06:31 -0400

    done

--- Program.cs
+++ Program.cs
@@ -8,10 +8,11 @@ namespace topic_6
         {
 
 
-            // bad();
-            // better();
-            // dice();
+            bad();
+             better();
+             dice();
             magic();
+            
         
         
         
@@ -29,8 +30,8 @@ namespace topic_6
             //BAD GUESSING GAME
             Random Generator = new Random();
             int randnum, min, max;
-            Console.WriteLine("names dave partner il guess what your thinking");
-            Console.WriteLine("enter a minumum number");
+            Console.WriteLine("Names Dave partner il guess what your thinking");
+            Console.WriteLine("Enter a minumum number");
             int.TryParse(Console.ReadLine(), out min);
             Console.WriteLine("enter a max number");
             int.TryParse(Console.ReadLine(), out max);
@@ -43,7 +44,7 @@ namespace topic_6
             int randnum, pick;
             Random Genertor = new Random();
             randnum = Genertor.Next(0, 11);
-            Console.WriteLine(" welcome to the best guessing game of all time pick a number between 1 and 10");
+            Console.WriteLine(" Welcome to the best guessing game of all time pick a number between 1 and 10");
             int.TryParse(Console.ReadLine(), out pick);
              if (pick >= 10)
             {
@@ -68,22 +69,118 @@ namespace topic_6
         public static void dice()
         {
             int dice1, dice2, sum;
-            
-            Random Genertor = new Random();
-            dice1 = Genertor.Next(0, 7);
-            dice2 = Genertor.Next(0, 7);
-            sum = dice1 + dice2;
-            Console.WriteLine(" You rolled " + dice1 + " and " + dice2 + " so you would have " + sum);
-            
+            string ans;
+            Console.WriteLine("Welcome to the dice roller you wanna roll? \n yes/no");
+            ans = Console.ReadLine().ToUpper();
+            if ( ans  == "YES"){
+
+
+                Random Genertor = new Random();
+                dice1 = Genertor.Next(0, 7);
+                dice2 = Genertor.Next(0, 7);
+                sum = dice1 + dice2;
+                Console.WriteLine(" You rolled " + dice1 + " and " + dice2 + " so you would have " + sum);
+            }
+
+            else if ( ans == "NO")
+            {
+                Console.WriteLine("WHAT WHY???????");
+            }
+        
+        
         }
       
         public static void magic()
         {
+            string ask;
+            int ball;
+            Random Genertor = new Random();
+            Console.WriteLine("Welcome to the magic 8 ball");
+             Console.WriteLine("ask any yes/no question");
+            ask = Console.ReadLine(); 
+            ball = Genertor.Next(0,11);
+            // good answers
+            if (ball == 1)
+            {
+               
+                Console.WriteLine("It is certain");
+            }
+            else if (ball == 2)
+            {
+                
+                Console.WriteLine("Yes deffintly");
+            }
+            else if (ball == 3)
+            {
+               
+                Console.WriteLine("Without a doubt");
+          
+       
+            }
+
+            else if (ball == 4)
+            {
+                
+                Console.WriteLine("Outlook good");
+            }
+            //medium
+            else if (ball == 5)
+            {
+                
+                Console.WriteLine("Ask again later");
+               
+            }
+            else if (ball == 6)
+            {
+                
+                Console.WriteLine("Answer hazy");
+            }
+            // bad
+            else if (ball == 7)
+            {
+              
+                Console.WriteLine("Dont count on it");
+            }
+            else if (ball == 8)
+            {
+                
+                Console.WriteLine("My reply is no");
+            }
+            else if (ball == 9)
+            {
+                
+                Console.WriteLine("Verry doubtfull");
+            }
+            else if (ball == 10)
+            {
+                
+                Console.WriteLine("Outlook not so good");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
-    
-    
-    
+
+
+
+
+
+
+
+
+
+       
+         
     
     } 
 
